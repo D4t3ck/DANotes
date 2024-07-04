2a552135cbaab484048ecb0e6dcc1989e4b385d4
daniel sinteck <d.sint3ck@gmail.com>
2024-07-04 12:30:53 +0200

    ++ read collectionData;

--- src/app/firebase-services/note-list.service.ts
+++ src/app/firebase-services/note-list.service.ts
@@ -1,31 +1,43 @@
 import { inject, Injectable } from '@angular/core';
-import { Firestore, collectionData, collection, doc } from '@angular/fire/firestore';
+import {
+  Firestore,
+  collectionData,
+  collection,
+  doc,
+} from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
 import { Note } from '../interfaces/note.interface';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class NoteListService {
-
   trashnotes: Note[] = [];
   normalNotes: Note[] = [];
 
   firestore: Firestore = inject(Firestore);
+  items$: Observable<any[]>;
+  afAuth: any;
 
-  constructor() { }
+  constructor() {
+    // const Collection = collection(this.firestore, 'items');
+    this.items$ = collectionData(this.getNotesRef());
+    /* console.log(this.firestore); */
+    
+  }
 
-  // const itemCollection = collection(this.firestore, 'items');
+  
 
   getNotesRef() {
     return collection(this.firestore, 'notes');
   }
+  
 
   getTrashRef() {
     return collection(this.firestore, 'trash');
   }
 
-  getsingleDocRef(colId:string, docId:string) {
+  getsingleDocRef(colId: string, docId: string) {
     return doc(collection(this.firestore, colId), docId);
   }
 }
