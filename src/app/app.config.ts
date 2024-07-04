021abc559d45507f5f718e554a523227a18a3e5b
daniel sinteck <d.sint3ck@gmail.com>
2024-07-04 11:46:48 +0200

    ++ initialised firestore, ref functions;

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,22 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes),
+    provideFirebaseApp(() =>
+      initializeApp({
+        projectId: 'danotes-4768a',
+        appId: '1:342046917566:web:1a1e7d0859612c2be7efef',
+        storageBucket: 'danotes-4768a.appspot.com',
+        apiKey: 'AIzaSyARoE8lDHxkH9ZQXCUkPhIDFgKiAkR6by4',
+        authDomain: 'danotes-4768a.firebaseapp.com',
+        messagingSenderId: '342046917566',
+      })
+    ),
+    provideFirestore(() => getFirestore()),
+  ],
 };
--- src/app/firebase-services/note-list.service.ts
+++ src/app/firebase-services/note-list.service.ts
@@ -1,9 +1,31 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
+import { Firestore, collectionData, collection, doc } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+import { Note } from '../interfaces/note.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NoteListService {
 
+  trashnotes: Note[] = [];
+  normalNotes: Note[] = [];
+
+  firestore: Firestore = inject(Firestore);
+
   constructor() { }
+
+  // const itemCollection = collection(this.firestore, 'items');
+
+  getNotesRef() {
+    return collection(this.firestore, 'notes');
+  }
+
+  getTrashRef() {
+    return collection(this.firestore, 'trash');
+  }
+
+  getsingleDocRef(colId:string, docId:string) {
+    return doc(collection(this.firestore, colId), docId);
+  }
 }
